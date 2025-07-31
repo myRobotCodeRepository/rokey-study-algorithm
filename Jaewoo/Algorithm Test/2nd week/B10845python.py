6691bc92b4ea9c529919763b4603a2220786660b
Jaewoo Kim <kjwmechacont@gmail.com>
2025-07-31 19:59:00 +0900

    2주차 세 번째 문제 완료

--- Jaewoo/Algorithm Test/2nd week/B10845python.py
+++ Jaewoo/Algorithm Test/2nd week/B10845python.py
@@ -2,24 +2,25 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-dummyStack = []
-for i in range(n):
-	commands = input().split()
-	if commands[0] == 'push': dummyStack.append(int(commands[1]))
-	elif commands[0] == 'pop': 
-		if not dummyStack:
-			print(-1)
-		else:
-			print(dummyStack.pop(0))
-	elif commands[0] == 'size': print(len(dummyStack))
-	elif commands[0] == 'empty': print(int(not dummyStack))
-	elif commands[0] == 'front':
-		if not dummyStack:
-			print(-1)
-		else:
-			print(dummyStack[0])
-	elif commands[0] == 'back':
-		if not dummyStack:
-			print(-1)
-		else:
-			print(dummyStack[-1])
+
+for _ in range(n):
+	dummyStack = []
+	string = input()
+	try:
+		for c in string:
+			if c == '(':
+				dummyStack.append(c)
+			elif c == ')':
+				if dummyStack[-1] == '(':
+					dummyStack.pop()
+				else:
+					print("NO")
+					break
+	except IndexError:
+		print("NO")
+		continue			
+	
+	if not dummyStack: print("YES")
+	else: print("NO")
+
+            
